dd21ac7c7688c47d1f463e482f16a77e77b49a5c
captnBill <noe.grandfond@gmail.com>
2022-06-14 15:51:34 +0200

    Création page admin

---
+++ src/pages/Admin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const AdminView = () => { 
+
+    return ( 
+        <Container>
+            <Row>
+                <Col>admin</Col> 
+                <Col></Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default AdminView
