33d3a13fbbdacb7795af400beb6f5973bdf77f86
AssA <gravelteo.pro@gmail.com>
2022-06-16 10:59:15 +0200

    CSV fini

--- src/pages/ExportToCSV.jsx
+++ src/pages/ExportToCSV.jsx
@@ -8,18 +8,22 @@ const ExportToCSV = () => {
     const exportToCSV = (data, fileName) => {
         const csvData = data
             .map((group) => {
-                console.log(group);
+                const groupInfo = [group.id, group.name];
+                const groupStudents = group.students.map((student) => {
+                    return [
+                        student.lastName,
+                        student.surname,
+                        student.grades.map((grade) => {
+                            return [grade.score, grade.subject.name];
+                        }),
+                    ];
+                });
 
-                return group.name;
-
-                // return group.map((item) => {
-                // return item.name;
-                // });
+                return [...groupInfo, ...groupStudents];
             })
-            .join("\n\n");
+            .join("\n");
 
-        const csv = `${csvData}`;
-        const blob = new Blob([csv], { type: "text/csv" });
+        const blob = new Blob([csvData], { type: "text/csv" });
         const url = URL.createObjectURL(blob);
         const link = document.createElement("a");
         link.href = url;
@@ -29,12 +33,48 @@ const ExportToCSV = () => {
     };
 
     React.useEffect(() => {
-        axios.get("http://172.19.2.11:5000/api/Group/GetGroups/").then((res) => {
-            setData(res.data);
+        axios.get("http://172.19.2.11:5000/api/Group/GetGroup/1").then((res) => {
+            setData(() => [res.data]);
         });
     }, []);
 
     console.log(data);
+
+    // const [gradeId, setGradeId] = React.useState();
+
+    // const deleteGrade = (id) => {
+    //     axios
+    //         .delete(`http://172.19.2.11:5000/api/Grade/DeleteGrade/${id}`)
+    //         .then((res) => {
+    //             console.log(res);
+
+    //             if (res.request.status === `${/20[0-9]/}`) {
+    //                 alert("Note supprimée avec succès");
+    //                 window.location.reload();
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    // };
+
+    // const updateGrade = (e) => {
+    //     e.preventDefault();
+    //     axios
+    //         .put("http://172.19.2.11:5000/api/Grade/UpdateGrade/", {
+    //             score: e.target.score.value,
+    //             studentId: id,
+    //             subjectId: subjectSelect,
+    //         })
+    //         .then((res) => {
+    //             console.log(res);
+
+    //             if (res.request.status === `${/20[0-9]/}`) {
+    //                 alert("Note modifiée avec succès");
+    //                 window.location.reload();
+    //             }
+    //         });
+    // };
 
     return (
         <div>
