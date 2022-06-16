69bd8bc9c95619d0bcc3622efa60a9aabf18ec9b
Azzarim <85098756+Azzarim@users.noreply.github.com>
2022-06-16 13:19:03 +0200

    Interface_Student_without_KPI

--- src/pages/Notes/DashboardStudent.js
+++ src/pages/Notes/DashboardStudent.js
@@ -1,55 +1,93 @@
 import React from 'react'
 import axios from 'axios'
 import { Container, Row, Col, Table } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 
 
 const NotesStudentDashboardView = () => {
+    const { id } = useParams();
+
     const [data, setData] = React.useState([])
+    const [note, setNote] = React.useState([])
+
+    const calculateAverage = (notesArray) => {
+        let sum = 0;
+        notesArray.forEach((note) => {
+            sum += note.score;
+        });
+        return sum / notesArray.length;
+    };
     
 
     React.useEffect(() => {
-        axios.get(`http://172.19.2.11:5000/api/Student/GetStudent/2`).then((res) => {
+        axios.get(`http://172.19.2.11:5000/api/Grade/GetStudentGrades/${id}`).then((res) => {
             console.log(res.data);
-            setData(res.data);
+            setNote(res.data);
         });
-        //fetchData()
+        axios.get(`http://172.19.2.11:5000/api/Student/GetStudent/${id}`).then((res) => {
+            console.log(res.data);
+            setData([res.data]);
+            console.log(data);
+        });
     }, [])
-
+ 
+        
     return (
         <Container fluid>
             <Row>
                 <Col>
-                    <Container>
+                    <Container className="p-3">
                         <Table responsive bordered hover striped size="sm">
                             <thead>
                                 <tr>
                                     <th>Nom</th>
-                                    <th>Prenom</th>
-                                    <th>Moyenne generale</th>
+                                    <th>Prénom</th>
+                                    <th>Moyenne générale</th>
                                 </tr>
                             </thead>
                             <tbody>
-                            {data.map((element) => {
-                                <tr key={element.id} value={element.id} >
-                                    <td>
-                                        {element.lastname}
-                                    </td>
-                                    <td>
-                                        {element.firstname}
-                                    </td>
-                                    <td>
-                                        {element.averageGrade}
-                                    </td>
-                                </tr>
-                                    
-                                })}
+                                {data.map((element, index) => {
+                                    return <tr key={index}>
+                                        <td>
+                                            {element.lastName}
+                                        </td>
+                                        <td>
+                                            {element.surname}
+                                        </td>
+                                        <td>
+                                            {element.grades.length > 0 ? `${calculateAverage(element.grades).toFixed(2)}/20` : "Aucune note"}
+                                        </td>
+                                        
+                                    </tr>
+                                }
+                                )}
                             </tbody>
                         </Table>
                     </Container>
                 </Col>
                 <Col>
                     <Container className="p-3">
-                          
+                    <Table responsive bordered hover striped size="sm">
+                            <thead>
+                                <tr>
+                                    <th>Matière</th>
+                                    <th>Note</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {note.map((notes, index) => {
+                                return <tr key={index}>
+                                        <td>
+                                            {notes.subject.name} 
+                                        </td>
+                                        <td> 
+                                            {notes.score}/20
+                                        </td>
+                                </tr>
+                            })}
+                            </tbody>
+                        </Table>
+
                     </Container>
                 </Col>
             </Row>
@@ -57,5 +95,6 @@ const NotesStudentDashboardView = () => {
         </Container>
     )
 }
+
 
 export default NotesStudentDashboardView
